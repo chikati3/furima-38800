837a6b4e60bc6db9f171fa761fc9e5cdea3242b8
chikati3 <chikazawakazuhiro3@gmail.com>
2023-03-07 16:09:23 +0900

    userテーブル、カラム作成

--- db/migrate/20230307062623_devise_create_users.rb
+++ db/migrate/20230307062623_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname, null: false, default: ""
+      t.string :family_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.string :family_name_kana, null: false, default: ""
+      t.string :last_name_kana, null: false, default: ""
+      t.date :birthday, null: false, default: "1930-01-01"
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2023_03_07_062623) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
+    t.string "family_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.date "birthday", default: "1930-01-01", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
