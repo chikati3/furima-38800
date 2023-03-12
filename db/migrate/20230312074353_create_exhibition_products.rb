3efb00c932642faa5b2b87c3d85f02ce6d37b173
chikati3 <chikazawakazuhiro3@gmail.com>
2023-03-12 16:55:12 +0900

    出品テーブル作成

--- db/migrate/20230312074353_create_exhibition_products.rb
+++ db/migrate/20230312074353_create_exhibition_products.rb
@@ -1,6 +1,15 @@
 class CreateExhibitionProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :exhibition_products do |t|
+      t.string        :product_name, null: false
+      t.text          :description_item, null: false
+      t.integer       :category_id, null: false
+      t.integer       :commodity_condition_id, null: false
+      t.integer       :delivery_charge_id, null: false
+      t.integer       :delivery_area_id, null: false
+      t.integer       :delivery_day_id, null: false
+      t.integer       :price, null: false
+      t.references    :user, null: false, foreign_key: true
 
       t.timestamps
     end
