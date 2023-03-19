9437d1e08699c36e91d288da4240bed0bd312d89
chikati3 <chikazawakazuhiro3@gmail.com>
2023-03-19 12:22:41 +0900

    出品ページ、ログイン、ログアウトページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
   
   def index
   end
@@ -21,6 +22,12 @@ class ItemsController < ApplicationController
 
   def product_params
     params.require(:exhibition_product).permit(:image, :product_name, :description_item, :category_id, :commodity_condition_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
   end
   
 end
