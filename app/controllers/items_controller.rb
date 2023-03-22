f66fc15798c9829b19a244510795dfcd315ace4c
chikati3 <chikazawakazuhiro3@gmail.com>
2023-03-22 17:22:00 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
-    @exhibition_products = ExhibitionProduct.all.order(created_at: :DESC)
+    @exhibition_products = ExhibitionProduct.order(created_at: :DESC)
   end
 
   def new
