e8843f533b30e72290c01e9d7918c3a33390c35a
chikati3 <chikazawakazuhiro3@gmail.com>
2023-03-26 13:29:08 +0900

    商品情報編集機能、実装

--- app/models/exhibition_product.rb
+++ app/models/exhibition_product.rb
@@ -19,6 +19,6 @@ class ExhibitionProduct < ApplicationRecord
   validates :delivery_day_id,  numericality: { other_than: 1 , message: "can't be blank" }
   
   validates :price, presence: true
-  validates :price, numericality:  { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality:  { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, allow_blank: true
   validates :price, numericality:  { only_integer: true}
 end
