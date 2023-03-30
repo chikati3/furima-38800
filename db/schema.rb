8bfbf8c0acb4af8a35f5cbd9fb90b5cabff4ddbe
chikati3 <chikazawakazuhiro3@gmail.com>
2023-03-30 18:50:54 +0900

    配送先、完売のモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_14_072529) do
+ActiveRecord::Schema.define(version: 2023_03_30_094232) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,28 @@ ActiveRecord::Schema.define(version: 2023_03_14_072529) do
     t.index ["user_id"], name: "index_exhibition_products_on_user_id"
   end
 
+  create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "delivery_area_id", null: false
+    t.string "municipalities", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "telephone_number", null: false
+    t.bigint "sold_out_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sold_out_id"], name: "index_shipping_addresses_on_sold_out_id"
+  end
+
+  create_table "sold_outs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "exhibition_product_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["exhibition_product_id"], name: "index_sold_outs_on_exhibition_product_id"
+    t.index ["user_id"], name: "index_sold_outs_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "family_name", default: "", null: false
@@ -68,4 +90,7 @@ ActiveRecord::Schema.define(version: 2023_03_14_072529) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "exhibition_products", "users"
+  add_foreign_key "shipping_addresses", "sold_outs"
+  add_foreign_key "sold_outs", "exhibition_products"
+  add_foreign_key "sold_outs", "users"
 end
